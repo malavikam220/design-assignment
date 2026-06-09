1627bb9dd1e8b788223a4d8d70b9eab4510cf4d3
Malavika M <devikamalavika216688@gmail.com>
2026-06-10 00:14:50 +0530

    Add files via upload

---
+++ day2/Universal_shift_register/Testbench/usr_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 15:39:12
+// Design Name: 
+// Module Name: usr_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module usr_tb(
+
+    );
+endmodule
