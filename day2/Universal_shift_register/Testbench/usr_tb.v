c0b8146674fea798426cbc30c091b608dc1a2021
Malavika M <devikamalavika216688@gmail.com>
2026-06-10 00:15:52 +0530

    Update usr_tb.v

--- day2/Universal_shift_register/Testbench/usr_tb.v
+++ day2/Universal_shift_register/Testbench/usr_tb.v
@@ -20,7 +20,43 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module usr_tb(
+module usr_tb;
 
-    );
+reg clk_tb;
+reg [1:0] mod_tb;
+reg sr_in_tb, sl_in_tb;
+reg [3:0] d_tb;
+wire [3:0] q_tb;
+
+usr dut(clk_tb, mod_tb, sr_in_tb, sl_in_tb, d_tb,q_tb);
+initial 
+    begin
+    {clk_tb, mod_tb, sr_in_tb, sl_in_tb,d_tb}=0;
+    end
+    always #5 clk_tb = ~clk_tb;
+
+initial
+begin
+
+    // Parallel Load
+    mod_tb = 2'b11;
+    d_tb= 4'b1010;
+    #10;
+
+    // Hold
+    mod_tb = 2'b00;
+    #10;
+
+    // Shift Right
+    mod_tb= 2'b01;
+    sr_in_tb= 1'b1;
+    #10;
+
+    // Shift Left
+    mod_tb = 2'b10;
+    sl_in_tb = 1'b0;
+    #10;
+
+end
+
 endmodule
