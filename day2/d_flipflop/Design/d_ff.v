31e0447787ac406ec8e998d264c26a0286db200d
Malavika M <devikamalavika216688@gmail.com>
2026-06-09 23:12:03 +0530

    Add files via upload

---
+++ day2/d_flipflop/Design/d_ff.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 12:34:45
+// Design Name: 
+// Module Name: d_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_ff(input d,rst,clk,output reg q,qbar
+
+    );
+    always @(posedge clk) 
+    begin
+        if (rst) begin
+        q<=1'b0;
+        qbar<=1'b1;
+        end
+    else begin q<=d; 
+    qbar<=~d;
+    end
+    end
+endmodule
